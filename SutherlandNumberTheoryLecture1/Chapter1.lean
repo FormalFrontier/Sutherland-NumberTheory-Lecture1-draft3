8399904e2e71647c3fbd5493d7d3ebbf501c33b8
Kim Morrison <477956+kim-em@users.noreply.github.com>
2026-04-21 14:59:06 +1000

    feat: restack Chapter1/01_18_Proof repair (progress/2026-04-21T04-56-44Z.md) (#228)

--- SutherlandNumberTheoryLecture1/Chapter1.lean
+++ SutherlandNumberTheoryLecture1/Chapter1.lean
@@ -24,6 +24,7 @@ import SutherlandNumberTheoryLecture1.Chapter1.«01_12_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_13_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_17_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_18_Proposition»
+import SutherlandNumberTheoryLecture1.Chapter1.«01_18_Proof»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_19_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_20_Proposition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_20_Proof»
