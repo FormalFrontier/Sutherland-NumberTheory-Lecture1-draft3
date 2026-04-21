14fc95d5e26960e7da87f16a28bfbf4f8a9d2e82
Kim Morrison <kim@tqft.net>
2026-04-21 05:49:43 +0000

    feat: scaffold Chapter1/01_27_Remark
    Progress: progress/2026-04-21T05-49-22Z_e1c3990c.md

--- SutherlandNumberTheoryLecture1/Chapter1.lean
+++ SutherlandNumberTheoryLecture1/Chapter1.lean
@@ -36,6 +36,7 @@ import SutherlandNumberTheoryLecture1.Chapter1.«01_23_Corollary»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_23_Proof»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_25_Proposition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_26_Definition»
+import SutherlandNumberTheoryLecture1.Chapter1.«01_27_Remark»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_28_Proposition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_28_Proof»
 
