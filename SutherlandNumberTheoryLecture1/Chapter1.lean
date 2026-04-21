d38ba15ff595596da69b255c3d539ae14e3cc485
Kim Morrison <kim@tqft.net>
2026-04-21 03:50:03 +0000

    feat: scaffold 01_05 proof for progress/2026-04-21T03-49-49Z.md

--- SutherlandNumberTheoryLecture1/Chapter1.lean
+++ SutherlandNumberTheoryLecture1/Chapter1.lean
@@ -9,6 +9,7 @@ import SutherlandNumberTheoryLecture1.Chapter1.«01_02_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_03_Example»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_04_Lemma»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_05_Corollary»
+import SutherlandNumberTheoryLecture1.Chapter1.«01_05_Proof»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_06_Definition»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_06a_Discussion»
 import SutherlandNumberTheoryLecture1.Chapter1.«01_07_Definition»
